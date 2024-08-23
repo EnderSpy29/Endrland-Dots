5bdb2a0126cd4cda511111c4d810442b89c07952
EnderSpy29 <theenderspy29@gmail.com>
2024-08-23 17:51:15 +1200

    add bind and script to screenshot all monitors

--- hypr/scripts/screenshot-all.sh
+++ hypr/scripts/screenshot-all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# A screenshot script
+# A screenshot all script
 
 tmpfile=$(mktemp)
 time=$(date +"%d-%m-%Y_%H:%M:%S.%3N")
 dir="$HOME/Pictures/Screenshots"
 file="Screenshot_${time}.png"
 
-grim -g "$(slurp)" - >"$tmpfile"
+grim - >"$tmpfile"
 if [[ -s "$tmpfile" ]]; then
 wl-copy <"$tmpfile"
 mv "$tmpfile" "$dir/$file"
---
+++ hypr/scripts/screenshot-select.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# A selection screenshot script
+
+tmpfile=$(mktemp)
+time=$(date +"%d-%m-%Y_%H:%M:%S.%3N")
+dir="$HOME/Pictures/Screenshots"
+file="Screenshot_${time}.png"
+
+grim -g "$(slurp)" - >"$tmpfile"
+if [[ -s "$tmpfile" ]]; then
+wl-copy <"$tmpfile"
+mv "$tmpfile" "$dir/$file"
+fi
